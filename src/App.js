9e700d7b5c41d5a201a4ed0e5135467f5c9a3855
Marius Duboule <dev@mariusduboule.com>
2022-05-13 22:08:49 -0400

    Create new Die component and add randomizer function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import Die from './Die'
+
 export default function App() {
-  return null
+  // Return an array of 10 random value betwee 1 and 6
+  function allNewDice() {
+    return [...Array(10)].map(value => {
+      return Math.floor(Math.random() * 6) + 1
+    })
+  }
+
+  const value = 4
+
+  return (
+    <main>
+      <div className="dices">
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+        <Die value={value} />
+      </div>
+    </main>
+  )
 }
